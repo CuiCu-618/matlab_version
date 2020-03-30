a6bc8b6a7534544abc2a73aecc3a07ee5b1f0243
Cui Cu <prc_cc@163.com>
2020-03-30 19:22:05 +0800

    3D example works

--- chap04/p42.m
+++ chap04/p42.m
@@ -4,19 +4,19 @@
 % !             elements in 2- or 3-dimensions
 % !-------------------------------------------------------------------------
 %% ---------------------------initialisation-------------------------------
-fixed_freedoms = 0;
+fixed_freedoms = 1;
 loaded_nodes = 1;
-ndim = 2;                       % number of dimensions
-nels = 10;
+ndim = 3;                       % number of dimensions
+nels = 4;
 nod = 2;
-nnprops = 1;
+nprops = 1;
 np_types = 1;
-nr = 2;
+nr = 4;
 penalty = 1e20;
 
 nodof = ndim;
 ndof = nod*nodof;
-nn = 6;
+nn = 5;
 
 nf = zeros(nodof,nn);
 km = zeros(ndof,ndof);
@@ -31,13 +31,16 @@ g_g = zeros(ndof,nels);
 etype = zeros(nels,1);
 prop = zeros(nprops,np_types);
 
-prop(:) = 2e5;
+prop(:) = 5e5;
 etype(:) = 1;
-g_coord(:,:) = [0,4,4,8,8,12;3,0,3,3,0,0];
-g_num(:,:) = [1,1,3,3,3,2,2,5,4,5;2,3,4,5,2,4,5,4,6,6];
+% g_coord(:,:) = [0,4,4,8,8,12;3,0,3,3,0,0];
+% g_num(:,:) = [1,1,3,3,3,2,2,5,4,5;2,3,4,5,2,4,5,4,6,6];
+g_coord(:,:) = [0,1.25,3.5,4,2;0,3,2,1,1.5;0,0,0,0,3];
+g_num(:,:) = [1,2,3,4;5,5,5,5];
 nf(:,:) = 1;
-k = [1,2];
-nf(:,k) = [0,1;0,0];
+k = [1,2,3,4];
+% nf(:,k) = [0,1;0,0];
+nf(:,k) = [0,0,0,0;0,0,0,0;0,0,0,0];
 nf = formnf(nf);
 neq = max(max(nf));
 kdiag = zeros(neq,1);
@@ -64,13 +67,17 @@ for iel = 1:nels
     kv = fsparv(kv,km,g,kdiag);
 end
 %% !----------------------- loads and/or displacements---------------------
-k = 6;
-loads(nf(:,k)) = [0,-10]';
+k = 5;
+% loads(nf(:,k)) = [0,-10]';
+loads(nf(:,k)) = [20,-20,30]';
 if fixed_freedoms ~= 0
     node = zeros(fixed_freedoms,1);
-    no = zeros(fixed_freedom,1);
-    sense = zeros(fixed_freedom,1);
-    value = zeros(fixed_freedom,1);
+    node(:) = 5;
+    no = zeros(fixed_freedoms,1);
+    sense = zeros(fixed_freedoms,1);
+    sense(:) = 2;
+    value = zeros(fixed_freedoms,1);
+    value(:) = -0.0005;
     for i = 1:fixed_freedoms
         no(i) = nf(sense(i),node(i));
     end
@@ -83,14 +90,18 @@ loads = spabac(kv,loads,kdiag);
 fprintf("  Node       Displacement(s) \n")
 for k = 1:nn
     if nf(1,k) == 0
-        nf(1,k) = 10;
+        nf(1,k) = 4;
         loads(nf(1,k)) = 0;
     end
     if nf(2,k) == 0
-        nf(2,k) = 10;
+        nf(2,k) = 4;
         loads(nf(2,k)) = 0;
     end
-    fprintf("   %d   %13.4e  %13.4e\n",k,loads(nf(1,k)),loads(nf(2,k)))
+    if nf(3,k) == 0
+        nf(3,k) = 4;
+        loads(nf(3,k)) = 0;
+    end
+    fprintf("   %d   %13.4e  %13.4e  %13.4e\n",k,loads(nf(1,k)),loads(nf(2,k)),loads(nf(3,k)))
 end
 %% !----------------------retrieve element end actions---------------------
 fprintf(" Element Actions \n")
@@ -98,12 +109,12 @@ for iel = 1:nels
     num = g_num(:,iel);
     coord = g_coord(:,num)';
     g = g_g(:,iel);
-    g(g==0) = 10;
+    g(g==0) = 4;
     eld = loads(g);
     km = pin_jointed(prop(1,etype(iel)),coord);
     action = km*eld;
     axial = glob_to_axial(action,coord);
-    fprintf(" %d  %13.4e  %13.4e  %13.4e  %13.4e \n",...
-            iel,action(1),action(2),action(3),action(4))
+    fprintf(" %d  %13.4e  %13.4e  %13.4e  %13.4e %13.4e  %13.4e \n",...
+           iel,action(1),action(2),action(3),action(4),action(5),action(6))
     fprintf("     Axial force = %11.4e\n",axial)
 end
