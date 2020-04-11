839b4ca51f7e3a139e600424e6ba028e7dac630e
Cui Cu <prc_cc@163.com>
2020-04-11 12:33:36 +0800

    Update p71.m
    ex2 works

--- chap07/p71.m
+++ chap07/p71.m
@@ -8,8 +8,8 @@ nod = 2;
 nprops = 1;
 penalty = 1e20;
 
-nels = 3;
-nn = 4;
+nels = 6;
+nn = 5;
 np_types = 3;
 neq = nn;
 
@@ -23,10 +23,10 @@ kdiag = zeros(neq,1);
 loads = zeros(neq,1);
 disps = zeros(neq,1);         % net nodal inﬂow/outﬂow
 
-prop(:,:) = [4,3,2]';
-etypes(:,:) = [1,2,3];
-ell(:,:) = [1,1,1];
-g_num(:,:) = [1,2,3;2,3,4];
+prop(:,:) = [3,2,1]';
+etypes(:,:) = [1,3,2,2,1,3];
+ell(:,:) = [1,1,1.5,1.5,1,1];
+g_num(:,:) = [1,2,1,3,3,4;2,3,3,5,4,5];
 %% !---------------------loop the elements to find global arrays sizes-----
 for iel = 1:nels
     num = g_num(:,iel);
@@ -47,15 +47,15 @@ for iel = 1:nels
 end
 kvh = kv;
 %% !---------------------specify boundary values---------------------------
-loaded_nodes = 1;
-k = 1;
-loads(k) = -100;
-fixed_freedoms = 2;
+loaded_nodes = 2;
+k = [3,5];
+loads(k) = [75,10];
+fixed_freedoms = 1;
 if fixed_freedoms ~= 0
     node = zeros(fixed_freedoms,1);
-    node(:,:) = [2,4];
+    node(:,:) = [1];
     value = zeros(fixed_freedoms,1);
-    value(:,:) = [-10,10];
+    value(:,:) = [0];
     kv(kdiag(node)) = kv(kdiag(node)) + penalty;
     loads(node) = kv(kdiag(node)) .* value;
 end
